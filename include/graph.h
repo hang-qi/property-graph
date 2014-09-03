9fefc5b53c35f3d516e5f5ce4f3e119414da5050
Hang Qi <hangqi@ucla.edu>
2014-09-03 16:49:50 -0700

    Update Graph interface

--- include/graph.h
+++ include/graph.h
@@ -4,23 +4,29 @@
 #include <ostream>
 #include <sstream>
 #include <algorithm>
-// #include <stdexp>
+#include <functional>
 
 #include "property.h"
+
+typedef unsigned int VertexIdType;
 
 class GraphElement
 {
 public:
-    GraphElement(const std::string& id)
-        : id_(id) {}
+    GraphElement() {}
     virtual ~GraphElement() {}
 
-    virtual void Print(std::ostream& os) const = 0;
+    virtual void PrintSummary(std::ostream& os) const = 0;
 
     std::shared_ptr<PropertyValue>& operator[] (const std::string& key)
     {
         return dict_[key];
     }
+
+    // std::shared_ptr<PropertyValue> operator[] (const std::string& key) const
+    // {
+    //     return dict_[key];
+    // }
 
     bool hasProperty(const std::string& key)
     {
@@ -42,12 +48,11 @@ protected:
 
 protected:
     PropertyDict dict_;
-    std::string id_;
 };
 
 std::ostream& operator<< (std::ostream& os, const GraphElement& element)
 {
-    element.Print(os);
+    element.PrintSummary(os);
     return os;
 }
 
@@ -55,73 +60,86 @@ std::ostream& operator<< (std::ostream& os, const GraphElement& element)
 class Vertex : public GraphElement
 {
 public:
-    Vertex(const std::string& id)
-        : GraphElement(id) {}
+    Vertex(const VertexIdType& id)
+        : id_(id) {}
     virtual ~Vertex() {}
 
 public:
-    virtual void Print(std::ostream& os) const
+    virtual void PrintSummary(std::ostream& os) const
     {
         os << "Vertex: " << id_;
         GraphElement::PrintProperty(os);
     }
+
+protected:
+    VertexIdType id_;
 };
 
 class Edge : public GraphElement
 {
 public:
-    Edge(const std::string& id,
-         const std::string& end_1,
-         const std::string& end_2)
-        : GraphElement(id)
+    Edge(const VertexIdType& end_1,
+         const VertexIdType& end_2)
     {
         end_1_ = end_1;
         end_2_ = end_2;
     }
 
-    Edge(const std::string& end_1,
-         const std::string& end_2)
-        : GraphElement("")
-    {
-        std::stringstream ss;
-        ss << end_1 << "->" << end_2;
-        id_ = ss.str();
-        end_1_ = end_1;
-        end_2_ = end_2;
-    }
     virtual ~Edge() {}
 
 public:
-    virtual void Print(std::ostream& os) const
+    virtual void PrintSummary(std::ostream& os) const
     {
-        os << "Edge: " << id_;
-        os << " ( " << end_1_ << " -> " << end_2_ << " )";
+        os << "Edge: " << end_1_ << " -> " << end_2_;
         GraphElement::PrintProperty(os);
     }
 
 protected:
-    std::string end_1_;
-    std::string end_2_;
+    VertexIdType end_1_;
+    VertexIdType end_2_;
 };
+
+// A labeler is a label creator which takes a GraphElement object
+// and produces the label that can is readable to human especially
+// for debug and visualization purposes.
+typedef std::function<std::string(GraphElement&)> Labeler;
 
 class Graph : public GraphElement
 {
 public:
-    Graph(const std::string& id)
-        : GraphElement(id) {}
+    Graph() {}
     virtual ~Graph() {};
 
 public:
-    virtual void Print(std::ostream& os) const
+    virtual void PrintSummary(std::ostream& os) const
     {
-        os << "Graph: " << id_ << std::endl;
+        os << "Graph: " << std::endl;
         os << "  # edges:    " << std::endl;
         os << "  # vertexes: ";
         GraphElement::PrintProperty(os);
     }
 
-    void AddVertex(const Vertex& vertex) {};
-    void AddEdge(const Edge& edge) {};
+    void AddVertex(const Vertex& vertex) {}
+    void AddVertices(const std::vector<Vertex>& vertex) {}
+
+    void AddEdge(const Edge& edge) {}
+    void AddEdges(const std::vector<Edge>& edge) {}
+
+    std::vector<std::shared_ptr<Edge>> GetEdges(const VertexIdType& source,
+        const VertexIdType& dest){}
+    std::vector<std::shared_ptr<Edge>> GetInEdges(const VertexIdType& source){}
+    std::vector<std::shared_ptr<Edge>> GetOutEdges(const VertexIdType& dest){}
+
+    int GetInDegree(const VertexIdType& source) {}
+    int GetOutDegree(const VertexIdType& dest) {}
+
+    Graph GetNeighbors(const VertexIdType& vid, int levels) {}
+
+    void WriteGraphViz(std::ostream& os, Labeler VertexLabeler, Labeler EdgeLabeler)
+    {}
+
+    void LoadRDF(std::ostream& os, Labeler VertexLabeler, Labeler EdgeLabeler) {}
+    void WriteRDF(std::ostream& os, Labeler VertexLabeler, Labeler EdgeLabeler) {}
 
 protected:
     // vertex set
--- test/test_graph.cpp
+++ test/test_graph.cpp
@@ -5,25 +5,22 @@ int main()
 {
     std::cout << "Create two vertices and edges.\n";
 
-    Vertex v1("v1");
+    Vertex v1(1);
     v1["name"] = make_value<std::string>("human");
     v1["weight"] = make_value<float>(0.8);
     v1["type"] = make_value<std::string>("AND");
 
-    Vertex v2("v2");
+    Vertex v2(2);
 
-    Edge e("v1", "v2");
-
-    Edge e2("edge_with_id", "v1", "v2");
+    Edge e(1, 2);
 
     std::cout << "Print out vertex and edge info.\n";
     std::cout << v1 << std::endl;
     std::cout << v2 << std::endl;
     std::cout << e << std::endl;
-    std::cout << e2 << std::endl;
 
     std::cout << "Create graph.\n";
-    Graph g("graph");
+    Graph g;
     g["name"] = make_value<std::string>("spatio AOG");
     std::cout << g << std::endl;
 
